02228a88fd28fc96cbe4532ad2329e24a57438ed
Zack Steinkamp <zack@steinkamp.us>
2023-04-17 18:34:56 -0700

    forgot new files

---
+++ components/BackButton.jsx
@@ -0,0 +1,14 @@
+import { useRouter } from 'next/router'
+
+const BackButton = ({ label="<<< Back", className }) => {
+  const router = useRouter();
+
+  return (
+    <button type="button" onClick={() => router.back()} className={className}>
+      { label }
+    </button>
+  );
+};
+
+export default BackButton;
+
---
+++ markdoc/tags/backbutton.markdoc.ts
@@ -0,0 +1,9 @@
+import BackButton from '@/components/BackButton';
+
+export const backbutton = {
+  render: BackButton,
+  attributes: {
+    label: { type: String },
+    className: { type: String },
+  },
+};
