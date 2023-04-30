35f3145377227e4e26433452e5a08f8489e1081e
Zack Steinkamp <zack@steinkamp.us>
2023-04-30 14:34:23 -0700

    pop has his own site

--- components/SiteHeader.jsx
+++ components/SiteHeader.jsx
@@ -20,8 +20,8 @@ const SiteHeader = () => {
 
         <div className="trigger font-condensed">
           <Link className="page-link" href="/">Posts</Link>
-          <Link className="page-link" href="https://photos.steinkamp.us" target="_blank">Photos</Link>
-          <Link className="page-link" href="/pop">Pop&apos;s Pages</Link>
+          <Link className="page-link" href="https://photos.steinkamp.us/" target="_blank">Photos</Link>
+          <Link className="page-link" href="https://dick.steinkamp.us/">Pop&apos;s Pages</Link>
           <Link className="page-link" href="/music">Music</Link>
           <Link className="page-link" href="/music-tools">Plugins</Link>
           <Link className="page-link" href="/resume">Résumé</Link>
--- pages/pop/studeute/2008-10-06_hot-rod-deluxe.md
+++
@@ -1,9 +0,0 @@
----
-layout: post
-title: Hot Rod Deluxe
-date: "2008-10-06"
----
-
-A nice mention about the Ute in the latest issue of Hot Rod Deluxe by the editor, Jerry Pitt…
-
-![](/images/pop/studeute/HRD.jpg "HRD")
