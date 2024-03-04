d7fc33b27ac0e17aaca5a4561b95777efb2ca564
Zack Steinkamp <zack@steinkamp.us>
2024-03-03 17:21:30 -0800

    more fix

--- pages/music.tsx
+++ pages/music.tsx
@@ -43,11 +43,9 @@ const Songs: React.FC<SongsProps> = ({ data }) => {
           {song.bandcampId && (
             <iframe
               className='h-24 w-full rounded-lg border-none'
-              src={`https://bandcamp.com/EmbeddedPlayer/${
-                song.type || 'track'
-              }=${
-                song.bandcampId
-              }/size=large/bgcol=666666/linkcol=0687f5/tracklist=false/artwork=small/transparent=true/`}
+              src={`https://bandcamp.com/EmbeddedPlayer/${song.type || 'track'
+                }=${song.bandcampId
+                }/size=large/bgcol=666666/linkcol=0687f5/tracklist=false/artwork=small/transparent=true/`}
               seamless
             ></iframe>
           )}
@@ -67,7 +65,7 @@ const Songs: React.FC<SongsProps> = ({ data }) => {
         />
       </Head>
       <TableOfContents headings={headings} className='mt-[0.6rem]' />
-      <h1>Music I've Made</h1>
+      <h1>Music I&apos;ve Made</h1>
       <p>
         One of my hobbies is music-making. I like to record sounds and make
         instruments out of them, or design new sounds with synthesizers. I used
