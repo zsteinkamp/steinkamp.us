882e17ea3d0cffd1e00ec2704129c09ba404a743
Zack Steinkamp <zack@steinkamp.us>
2024-03-04 16:32:29 -0800

    speed up trans; align ToC

--- pages/music-tools.tsx
+++ pages/music-tools.tsx
@@ -67,7 +67,7 @@ const MusicToolsPage: React.FC<MusicToolsProps> = ({ data }) => {
           content='Tools that I have created for other musicians, mostly in Max for Live.'
         />
       </Head>
-      <TableOfContents headings={headings} className='mt-[0.6rem]' />
+      <TableOfContents headings={headings} className='mt-[0.5rem]' />
       <h1>Music Tools / Plugins</h1>
       <p>
         I have made a handful of tools for electronic musicians who use Ableton
--- pages/resume.md
+++ pages/resume.md
@@ -4,7 +4,7 @@ outerClass: resume
 tableOfContents: true
 tocMaxLevel: 3
 tocMinLevel: 2
-tocClassName: 'mt-[0.6rem]'
+tocClassName: 'mt-[0.5rem]'
 ---
 
 # Zack Steinkamp
