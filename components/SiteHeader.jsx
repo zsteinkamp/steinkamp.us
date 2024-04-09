51cf1a67b94116892d9eb4a225d5f7749e44c89f
Zack Steinkamp <zack@steinkamp.us>
2024-04-09 11:57:00 -0700

    tweaker

--- components/SiteHeader.jsx
+++ components/SiteHeader.jsx
@@ -18,7 +18,7 @@ const SiteHeader = ({ className = '' }) => {
       className={`noprint relative ml-0 mb-4 flex h-[4.5rem] items-center border-b-2 border-border pl-4 md:sticky md:top-0 md:ml-0 md:mr-4 md:w-[12rem] md:flex-col md:border-b-0 ${className}`}
     >
       <figure title='steinkamp.us' className=''>
-        <div className='absolute top-0 left-[-1rem] mr-2 w-12 md:top-[-6rem] md:left-0 md:mr-0 md:w-[13rem]'>
+        <div className='absolute top-0 left-[-1rem] mr-2 w-12 md:top-[-6.0rem] md:left-0 md:mr-0 md:w-[13rem]'>
           <Mono />
         </div>
         <div className='text-center md:mb-2 md:mt-0'>
